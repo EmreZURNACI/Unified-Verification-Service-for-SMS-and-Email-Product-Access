27a08b7405acb75ced2e510f0308f92705cbae60
Emre ZURNACI <emrezurnaci38@gmail.com>
2024-09-26 21:28:55 +0300

    Postgresql database link function created.

---
+++ Connection/Connection.go
@@ -0,0 +1,34 @@
+package connection
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	m "Models.go"
+	_ "github.com/lib/pq"
+)
+
+var ConfigModel m.ConfigFile
+
+func init() {
+	bs, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(bs, &ConfigModel)
+	if err != nil {
+		panic(err)
+	}
+}
+func Connection() *sql.DB {
+	var dsn string = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", ConfigModel.Dbmodel.Host, ConfigModel.Dbmodel.Port, ConfigModel.Dbmodel.User, ConfigModel.Dbmodel.Password, ConfigModel.Dbmodel.Dbname)
+	con, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	return con
+}
+
+//build dene
