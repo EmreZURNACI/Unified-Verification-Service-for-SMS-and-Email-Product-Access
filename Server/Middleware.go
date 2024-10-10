234a89985b842b9f53c7b6f26faa36a97d168779
Emre ZURNACI <emrezurnaci38@gmail.com>
2024-10-10 20:27:35 +0300

    a middleware is added that checks if functions are created in the database.

--- Server/Middleware.go
+++ Server/Middleware.go
@@ -1,29 +1,39 @@
-package server
+package Server
 
 import (
 	"fmt"
 	"net/http"
 
-	c "Connection.go"
-	helpers "Helpers.go"
+	c "ProductService/Connection"
+	"ProductService/Functions"
+	h "ProductService/Helpers"
 )
 
 func IsSetTokenMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("Token")
 		if err != nil || cookie == nil {
-			fmt.Fprintf(w, "%+v", helpers.Response(false, 401, "Unauthorized", nil))
+			fmt.Fprintf(w, "%+v", h.Response(false, 401, "Unauthorized", nil))
 		} else {
 			next.ServeHTTP(w, r)
 		}
 	})
 }
-
 func IsDatabaseConnected(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		err := c.Connection().Ping()
 		if err != nil {
-			fmt.Fprintf(w, "%+v", helpers.Response(false, 400, "Database Connection Was Crashed", nil))
+			fmt.Fprintf(w, "%+v", h.Response(false, 400, "Database Connection Was Crashed", nil))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+func IsFunctionsCreated(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		statu := Functions.StartFunctions()
+		if !statu {
+			fmt.Fprintf(w, "%+v", h.Response(false, 400, "Database functions could not be created.", nil))
 		} else {
 			next.ServeHTTP(w, r)
 		}
